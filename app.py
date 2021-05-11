3a30056a6cb78ab8dfeabd9cb6ac990a1ea79b91
ovyas24 <ovyas24@gmail.com>
2021-05-11 22:57:55 +0530

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask,request
 from flask import jsonify
 from Recommendation import generateRecomendations,getAllMovies
-# import requests
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
